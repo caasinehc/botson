b40ec07e194781c9b26c2527ef66c9f3381ecfee
caasinehc <31968674+caasinehc@users.noreply.github.com>
2020-09-05 22:09:12 -0400

    Botson v1.4
    Changes:
    + Added token usage for the Tenor API
    ~ Funnier
    ~ Now will request only SFW gifs, unless in a NSFW text channel
    ~ When sending multiple GIFs, they will now be broken into blocks of five per message instead of sending each one in a separate message

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Botson
-v1.1.0
+v1.4.0
 By Isaac Chen
-8/9/2020
+9/5/2020
 
 Special thanks to Michiel Mulders for his helpful article on SitePoint about creating a discord bot: [https://www.sitepoint.com/discord-bot-node-js/](https://www.sitepoint.com/discord-bot-node-js/)
 
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*
  * Botson
- * v1.2.0
+ * v1.4.0
  * By Isaac Chen
- * 8/10/2020
+ * 9/5/2020
  */
 
 // Requires
@@ -10,39 +10,66 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const HTTP = require("http");
 
-// Bot and token
-const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+// Tokens
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const TENOR_TOKEN = process.env.TENOR_TOKEN;
 
 // Settings
 const MAX_GIFS = 10;
 const caasinehcID = "195213987871195137";
 
+// Bot and token
+const bot = new Discord.Client();
+
 // Functions
-// Send an http request to get a random Emma Watson GIF from Tenor
-function getGIFURL() {
-	// Return a promise that will resolve to the GIF URL
-	return new Promise((resolve, reject) => {
+// Send an http request to get random Emma Watson GIF links from Tenor
+function getGIFURLs(count, PG_only = false) {
+	// Sanitize the count argument
+	if(!Number.isInteger(count)) {
+		throw "Parameter \"count\" must be an integer!";
+	}
+	
+	// Return a promise that will resolve to an array of GIF URLs
+	return new Promise(resolve => {
+		// Generate query parameters
+		const QUERY_PARAMS = {
+			key: TENOR_TOKEN,
+			q: "Emma%20Watson",
+			locale: "en_US",
+			contentfilter: PG_only ? "high" : "off",
+			media_filter: "minimal",
+			ar_range: "all",
+			limit: count
+		};
+		const QUERY_PARAM_STRING = (Object
+			.keys(QUERY_PARAMS)
+			.map(key => key + "=" + QUERY_PARAMS[key])
+			.join("&")
+		);
+		
 		// HTTP request options
 		const OPTIONS = {
 			host: "api.tenor.com",
-			path: "/v1/random?q=Emma%20Watson&limit=1"
+			path: "/v1/random?" + QUERY_PARAM_STRING
 		};
 		
-		// Callback for when we recieve data from the HTTP request
+		// Callback for when we receive data from the HTTP request
 		function callback(response) {
 			// Start with an empty string as the response
 			let responseStr = "";
 			
-			// When we recieve a chunk of data, append it to the end of the reponse string
+			// When we receive a chunk of data, append it to the end of the response string
 			response.on("data", chunk => {
 				responseStr += chunk;
 			});
 			
-			// When we have recieved the full response, resolve the promise and return it
+			// When we have received the full response, resolve the promise and return it
 			response.on("end", () => {
-				const WATSON_URL = JSON.parse(responseStr)["results"][0]["url"];
-				resolve(WATSON_URL);
+				const WATSON_URLS = (JSON
+					.parse(responseStr)["results"]
+					.map(gifObj => gifObj["url"])
+				);
+				resolve(WATSON_URLS);
 			});
 		}
 		
@@ -52,7 +79,7 @@ function getGIFURL() {
 }
 
 // Login the bot
-bot.login(TOKEN);
+bot.login(BOT_TOKEN);
 
 // When the bot is ready, log to the console
 bot.on("ready", () => {
@@ -69,7 +96,7 @@ bot.on("message", msg => {
 	// becho
 	else if(msg.content.toLowerCase().split(" ")[0] === "!becho") {
 		// Verify that caasinehc was the one who asked
-		if(msg.author.id === caasinehcID) {	
+		if(msg.author.id === caasinehcID) {
 			// Echo whatever comes after "!becho "
 			const msgToEcho = msg.content.substring("!becho ".length - 1);
 			if(msgToEcho.length > 0) {
@@ -90,22 +117,39 @@ bot.on("message", msg => {
 		const countInput = msg.content.split(" ")[1];
 		let count = parseInt(countInput);
 		// Ensure it is a number (default to 1 if it isn't)
-		if(Number.isNaN(count) || count < 1) {
+		if(Number.isNaN(count)) {
 			count = 1;
 		}
+		
+		// 69 420 lol funny original meme jokes
+		if(count === 69 || count === 420 || count === 69420) {
+			msg.channel.send(":rolling_eyes:");
+		}
 		// Ensure that we aren't sending too many GIFs at once
-		if(count > MAX_GIFS) {
-			msg.channel.send(`Sorry, I can only send up to ${MAX_GIFS} GIFs at a time!`);
+		else if(count > MAX_GIFS) {
+			msg.channel.send(`I don't really wanna send more than ${MAX_GIFS} GIFs at a time, sorry!`);
+		}
+		// Ensure that we aren't sending zero or a negative number of gifs
+		else if(count <= 0) {
+			msg.channel.send(`How am I supposed to send ${count} GIFs? I'm confused...`);
 		}
 		// If the number of GIFs requested is reasonable, send em!
 		else {
-			// However many times the user requested, send an Emma Watson GIF
-			for(let i = 0; i < count; i++) {
-				// Get GIF URL and send it
-				getGIFURL().then(result => {
-					msg.channel.send(result);
-				});
-			}
+			// Send the requested number of Emma Watson GIFs!
+			getGIFURLs(count, !msg.channel.nsfw).then(result => {
+				// Discord will only turn the first five links into embedded
+				// images, so we have to split the GIFs into blocks of 5, and
+				// send each block as a separate message
+				const blocks = [];
+				while(result.length > 0) {
+					blocks.push(result.splice(0, 5));
+				}
+				
+				// Send the message(s)
+				for(let block of blocks) {
+					msg.channel.send(block);
+				}
+			});
 		}
 	}
 });
